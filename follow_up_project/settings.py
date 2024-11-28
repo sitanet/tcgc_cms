ba03b4938a76536dc4355d185e7249ed535d3923
sitanet <sitanetglobaltech@gmail.com>
2024-11-28 11:38:09 +0100

    key

--- follow_up_project/settings.py
+++ follow_up_project/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-abl9ym&=_py-b-t^-5z8%+!#p%iresgkgtvbh#f82xv%jw7d#p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
