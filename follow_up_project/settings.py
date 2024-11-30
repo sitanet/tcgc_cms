661adaa677d1285932205385cf1205267a4d7a46
sitanet <sitanetglobaltech@gmail.com>
2024-11-30 16:50:09 +0100

    domainall

--- follow_up_project/settings.py
+++ follow_up_project/settings.py
@@ -35,7 +35,14 @@ SECRET_KEY = 'django-insecure-abl9ym&=_py-b-t^-5z8%+!#p%iresgkgtvbh#f82xv%jw7d#p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','207.154.197.128','.sitanetorbit.com','tcgcms.sitanetorbit.com','www.tcgcms.sitanetorbit.com','http://tcgcms.sitanetorbit.com/']
+ALLOWED_HOSTS = [
+    'localhost', 
+    '127.0.0.1', 
+    '207.154.197.128', 
+    'tcgcms.sitanetorbit.com', 
+    'www.tcgcms.sitanetorbit.com'
+]
+
 
 # CFSF_TRUSTED_ORIGINS = ['www.tcgcms.sitanetorbit.com', 'tcgcms.sitanetorbit.com']
 
